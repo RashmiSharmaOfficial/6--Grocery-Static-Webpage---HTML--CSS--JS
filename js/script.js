f9c796591f8710da2999ef271d8131194c240d2e
rashmisharmaofficial <rashmisharmaofficial19019@gmail.com>
2021-12-26 12:22:42 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// for bringng search bar
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+//for bringing shopping cart
+let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+//for bringing login form
+let loginForm = document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+//for 768px menu bar
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
